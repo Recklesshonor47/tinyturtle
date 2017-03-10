8e697f5257cc530c4656eab697364657f295b082
honor freeman <suisuiuchiha16@outlook.com>
2017-03-10 14:40:32 +0000

    my comment

---
+++ script.js
@@ -0,0 +1,17 @@
+/*global TinyTurtle*/
+TinyTurtle.apply(window); 
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+
+right(30);
+forward(40);
+right(120);
+forward(40);
+stamp(); 
